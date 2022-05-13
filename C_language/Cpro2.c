296a9e3c6e5a6da16e2d4ebd87d5b822339496e2
LeeHanEum <LeeHanEum160924@outlook.kr>
2022-05-13 19:56:17 +0900

    test

--- C_language/Cpro2.c
+++ C_language/Cpro2.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main (){
-    printf("hello");
+    int n, m;
+    int sum = 0;
+
+    printf("정수 n을 입력 : ");
+    scanf("%d", &n);
+
+    printf("정수 m을 입력 : ");
+    scanf("%d", &m);
+   
+    for (; n<=m ; n++)
+    {
+        sum += n;            
+    }
+
+    printf("n부터 m까지의 합은  : %d", sum);
+    
 
     return 0;
 }
